34946dfef0072d1a0661661825c08f353f32cca3
Ali <Ali@MacBook-Pro.local>
2020-08-21 16:39:34 -0300

    Added action creators for authentication

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,12 +1,20 @@
+//Ingredient actions:
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
 
+//Purchase actions:
 export const PURCHASE_START = 'PURCHASE_START';
 export const PURCHASE_SUCCESS = 'PURCHASE_SUCCESS';
 export const PURCHASE_FAIL = 'PURCHASE_FAIL';
 export const PURCHASE_INIT = 'PURCHASE_INIT';
 export const RESET = 'RESET';
 
+//Order actions:
 export const FETCH_ORDERS_START = 'FETCH_ORDERS_START';
 export const FETCH_ORDERS_SUCCESS = 'FETCH_ORDERS_SUCCESS';
 export const FETCH_ORDERS_FAIL = 'FETCH_ORDERS_FAIL';
+
+//Authentication actions:
+export const AUTH_START = 'AUTH_START';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
