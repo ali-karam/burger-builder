0100660c562d1d92404b017b03017a1445b97cd4
Ali <Ali@MacBook-Pro.local>
2020-08-21 18:57:13 -0300

    Added a spinner for loading and  error messages for authentication

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 
@@ -89,6 +90,18 @@ class Auth extends Component {
         });
     };
 
+    formatErrorMessage = (errorMessage) => {
+        switch(errorMessage) {
+            case 'EMAIL_EXISTS':
+                return 'Sorry the email already exists, please try a different email.';
+            case 'INVALID_PASSWORD':
+                return 'Sorry the password is incorrect, please try again.';
+            case 'WEAK_PASSWORD : Password should be at least 6 characters':
+                return 'Please enter at least 6 characters for the password.'
+            default: return 'Something went wrong';
+        }
+    };
+
     render () {
         const formElementsArray = [];
         for(let key in this.state.controls) {
@@ -98,7 +111,7 @@ class Auth extends Component {
             });
         }
 
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -111,18 +124,39 @@ class Auth extends Component {
                 changed={(event) => this.inputChangedHandler(event, formElement.id)}
             />
         ));
+
+        if(this.props.loading) {
+            form = <Spinner/>;
+        }
+
+        let errorMessage = null;
+        if(this.props.error !== null) {
+            errorMessage = (
+                <p>{this.formatErrorMessage(this.props.error.message)}</p>
+            );
+        }
+
         return (
             <div className={classes.Auth}>
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">Submit</Button>
                 </form>
-                <Button btnType="Danger" clicked={this.switchAuthModeHandler}>Switch to 
-                    {this.state.isSignup ? ' Sign In' : ' Sign Up'}</Button>
+                <Button btnType="Danger" clicked={this.switchAuthModeHandler}>
+                    Switch to {this.state.isSignup ? ' Sign In' : ' Sign Up'}
+                </Button>
+                {errorMessage}
             </div>
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+};
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -130,4 +164,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -41,8 +41,7 @@ export const auth = (email, password, isSignup) => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
             }).catch(err => {
-                console.log(err.response);
-                dispatch(authFail(err.response));
+                dispatch(authFail(err.response.data.error));
             }
         );
     };
