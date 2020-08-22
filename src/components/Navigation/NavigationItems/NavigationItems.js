4c23cb13aa88fce64a04e7c65f6c0a66fd1974c8
Ali <Ali@MacBook-Pro.local>
2020-08-21 21:01:06 -0300

    Updated the UI to display appropriate logout/sign in buttons

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,12 +2,19 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Authenticate</NavigationItem>
-    </ul>
-);
+const navigationItems = (props) => {
+    let authLink = <NavigationItem link="/auth">Sign Up/Sign In</NavigationItem>;
+    if(props.isAuthenticated) {
+        authLink = <NavigationItem link="/logout">Logout</NavigationItem>;
+    }
+
+    return(
+        <ul className={classes.NavigationItems}>
+            <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+            <NavigationItem link="/orders">Orders</NavigationItem>
+            {authLink}
+        </ul>
+    );
+};
 
 export default navigationItems;
