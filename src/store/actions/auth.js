0598bb2c1ee82565a0bc9c3f1f7f5674ade9d34f
Ali <Ali@MacBook-Pro.local>
2020-08-22 16:11:00 -0300

    Implemented the use of local storage to login the user automatically if they have a valid token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -53,6 +56,10 @@ export const auth = (email, password, isSignup) => {
         axios.post('https://identitytoolkit.googleapis.com/v1/accounts:' + 
             method + '?key=' + apiKey, authData).then(response => {
                 console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             }).catch(err => {
@@ -66,5 +73,18 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        const expirationDate = new Date(localStorage.getItem('expirationDate'));   
+        if(token !== null && expirationDate > new Date()) {
+            const userId = localStorage.getItem('userId');
+            dispatch(authSuccess(token, userId));
+            dispatch(checkAuthTimeout((expirationDate.getTime() - 
+                new Date().getTime()) / 1000));
+        } 
     };
 };
