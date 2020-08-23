0fedfdfd7d81ac353ac6eaceda3990177a1ce253
Ali <Ali@MacBook-Pro.local>
2020-08-23 15:23:27 -0300

    Added a guard against empty burger orders

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -8,14 +8,22 @@ import ContactData from '../Checkout/ContactData/ContactData';
 class Checkout extends Component {
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
-    }
+    };
 
     checkoutContinuedHandler = () => {
         this.props.history.replace('/checkout/contact-data');
-    }
+    };
+
+    emptyBurger = () => {
+        const sum = Object.values(this.props.ingredients).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        return sum <= 0;
+    };
 
     render() {
-        const purchaseRedirect = this.props.purchased ? <Redirect to="/"/> : null;
+        const purchaseRedirect = this.props.purchased || this.emptyBurger() ? 
+            <Redirect to="/"/> : null;
         return (
             <div>
                 {purchaseRedirect}
