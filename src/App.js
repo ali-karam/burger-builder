8e0d6de0ccb7ded9aacd75976a26d4ae20b9017d
Ali <Ali@MacBook-Pro.local>
2020-08-23 13:59:18 -0300

    Added guards for protected routes

--- src/App.js
+++ src/App.js
@@ -16,19 +16,36 @@ class App extends Component {
     }
 
     render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth} />
+                <Route path="/" component={BurgerBuilder} />
+            </Switch>
+        );
+
+        if(this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/logout" component={Logout} />
+                    <Route path="/" component={BurgerBuilder} />
+                </Switch>
+            );
+        }
         return (
             <div>
                 <Layout>
-                    <Switch>
-                        <Route path="/checkout" component={Checkout} />
-                        <Route path="/orders" component={Orders} />
-                        <Route path="/auth" component={Auth} />
-                        <Route path="/logout" component={Logout} />
-                        <Route path="/" component={BurgerBuilder} />
-                    </Switch>
+                    {routes}
                 </Layout>
             </div>
         );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
     }
 }
 
@@ -38,4 +55,4 @@ const mapDispatchToProps = (dispatch) => {
     };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
